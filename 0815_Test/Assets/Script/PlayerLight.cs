a47c5cfe01698dbe81a3de4babed055e7990ab9f
roomMaker <leehuk0622@gmail.com>
2022-08-16 16:16:36 +0900

    Feat : Make FlashLight

---
+++ 0815_Test/Assets/Script/PlayerLight.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLight : MonoBehaviour
+{
+    private UserInput _input;
+    private Light _light;
+
+    private void Awake()
+    {
+        _input = GetComponentInParent<UserInput>();
+        _light = GetComponent<Light>(); 
+    }
+
+    private void Update()
+    {
+        if(_input.IsLightOn)
+        {
+            _light.range = 35f;
+        }
+        else
+        {
+            _light.range = 10f;
+        }
+    }
+}
